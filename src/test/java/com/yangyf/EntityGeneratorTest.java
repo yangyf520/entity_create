385034925d9b667a5898331b10e3a7525cc4bcf7
杨雨锋 <yangyf>
2019-02-26 11:19:27 +0800

    oracle

---
+++ src/test/java/com/yangyf/EntityGeneratorTest.java
@@ -0,0 +1,57 @@
+package com.yangyf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class EntityGeneratorTest {
+
+    @Test
+    public void contextLoads() {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        String genCfg = "/generatorConfig.xml";
+        File configFile = new File(EntityGeneratorTest.class.getResource(genCfg).getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
